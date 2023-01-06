7fc268eac9273be65fba674ef7356def7e54edad
yonaim <yonaimyo@gmail.com>
2023-01-06 16:05:02 +0900

    Refactor: cmd_line -> lists_of_commands 변수명 변경

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 01:49:08 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/06 11:33:36 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/06 16:02:32 by yeonhkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ t_goldsh	g_goldsh;
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*cmd_line;
+	char	*lists_of_commands;
 
 	argc = 0;
 	argv = 0;
 	print_welcome();
 	while (1)
 	{
-		cmd_line = readline(PS1_DOLLAR);
+		lists_of_commands = readline(PS1_DOLLAR);
 		update_history();
 		wait_signal();
-		lexer(cmd_line, envp);
+		lexer(lists_of_commands, envp);
 		parser();
 		executor();
 		free(cmd_line);
