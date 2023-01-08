5be7f8e96a572c6b6fccd68873f475696f88da96
hey-uu <khy2714@gmail.com>
2023-01-09 08:38:22 +0900

    Refactor: minishell.h 구조체 수정

--- inc/mandatory/minishell.h
+++ inc/mandatory/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yona <yona@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 02:14:55 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/08 02:22:16 by yona             ###   ########.fr       */
+/*   Updated: 2023/01/09 08:02:12 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,31 @@
 
 /*-------------- USER DEFINED HEADERS ------------*/
 
-# include "vector.h"
+// # include "vector.h"
+# include "queue.h"
 # include "libft.h"
 
 /*------------ DEFINE MACRO CONSTANTS ------------*/
 
 # define SYNTAX_ERROR -1
+# define QUEUE_INITIAL_SIZE 10
 
 // subshell 실행(parenthesis)은 지금 구현할 건 아니지만 까먹을까봐 일단 적어두기만 함
 typedef enum e_token_type
 {
-	TOKEN_NONE,
-	TOKEN_WORD,
-	TOKEN_LPAREN,
-	TOKEN_RPAREN,
-	TOKEN_AND_IF,
-	TOKEN_OR_IF,
-	TOKEN_PIPE,
-	TOKEN_REDIR_IN,
-	TOKEN_REDIR_OUT,
-	TOKEN_REDIR_IN_HERE,
-	TOKEN_REDIR_OUT_APP
+	TOKEN_NONE = 0,
+	TOKEN_WORD = 1,
+	TOKEN_LPAREN = 2,
+	TOKEN_RPAREN = 3,
+	TOKEN_AND_IF = 4,
+	TOKEN_OR_IF = 5,
+	TOKEN_PIPE = 6,
+	TOKEN_REDIR_IN = 7,
+	TOKEN_REDIR_OUT = 8,
+	TOKEN_REDIR_IN_HERE = 9,
+	TOKEN_REDIR_OUT_APP = 10
 }	t_token_type;
+
 
 /*------------- STRUCT DECLARATIONS --------------*/
 
@@ -61,44 +64,66 @@ typedef struct s_token
 
 typedef enum e_node_type
 {
-	NODE_COMMAND,
-	NODE_SUBSHELL,
-	NODE_PIPE,
-	NODE_AND_IF,
-	NODE_OR_IF,
+	NODE_NONE = 0,
+	NODE_SIMPLE_COMMAND = 1,
+	NODE_SUBSHELL = 2,
+	NODE_PIPELINE = 3,
+	NODE_AND_IF = 4,
+	NODE_OR_IF = 5
 }	t_node_type;
 
 /**
- * @brief Pasor
+ * @brief Parsor
  * 
  */
 typedef struct s_simple_command
 {
-	char					*absolute_path;
-	char					**argument_list;
-	t_ivector				expansion_mask_list;
-	t_ivector				rdir_in;
-	t_ivector				rdir_out;
-	struct s_simple_command	*next;
+	char	*cmd_name;
+	t_queue	*argv;
+	t_queue	*redir_in;
+	t_queue	*redir_out;
 }	t_simple_command;
 
-typedef struct s_abstract_syntax_tree
+typedef struct s_subshell
 {
-	t_node_type						type;
-	t_simple_command				*simple_command;
-	char							**envp;
-	struct s_abstract_syntax_tree	*left;
-	struct s_abstract_syntax_tree	*right;
-}	t_abstract_syntax_tree;
+	int	check;
+}	t_subshell;
+
+typedef union u_command
+{
+	t_simple_command	*simple;
+	t_subshell			*subshell;
+}	t_command;
+
+typedef struct s_tree
+{
+	t_node_type			type;
+	t_command			content;
+	struct s_tree		*first_child;
+	struct s_tree		*next_sibling;
+}	t_tree;
+
+enum e_syntax_error
+{
+	NONE = 0,
+	
+};
+typedef enum e_syntax_error t_stx_err;
 
 typedef struct s_goldsh
 {
-	t_token					*token;
-	t_abstract_syntax_tree	tree;
+	char		**envp;
+	t_token		*token;
+	t_tree		*tree;
+	t_stx_err	syntax_error;
 }	t_goldsh;
+
+t_goldsh	g_goldsh;
 
 /*-------------- FUNCTION PROTOTYPES -------------*/
 
 t_token	*lexer(char *input);
+t_tree	*parser(t_token *tokens);
+void	destroy_parse_tree(t_tree *node);
 
 #endif
