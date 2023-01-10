52966a7f9377596e36916f0cd55a9b442fdf97f1
hey-uu <khy2714@gmail.com>
2023-01-10 20:52:31 +0900

    Refactor: libft_internal.h (v1.0)

--- lib/libft/libft_internal.h
+++ lib/libft/libft_internal.h
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   libft_internal.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 14:04:52 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/10 14:34:47 by yeonhkim         ###   ########.fr       */
+/*   Updated: 2023/01/10 20:51:25 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_INTERNAL_H
 # define LIBFT_INTERNAL_H
 
-# include <stdlib.h>
-# include <unistd.h>
+/*--------------- STANDARD HEADERS ---------------*/
+
+# include <sys/types.h>
+
+/*------------ DEFINE MACRO CONSTANTS ------------*/
 
 # ifndef FT_SIZE_MAX
 #  define FT_SIZE_MAX 9223372036854775807
@@ -24,62 +27,39 @@
 #  define FT_LONG_MAX 9223372036854775807L
 # endif
 
-typedef enum e_libft_error
+enum e_libft_error
 {
 	NORMAL = 0,
 	BADALLOC,
 	OVERSIZE,
 	BADENVP
-}	t_libft_error;
+};
 
-typedef enum e_bool
+enum e_bool
 {
 	TRUE = 1,
 	FALSE = 0
-}	t_bool;
+};
+
+/*------------- STRUCT DECLARATIONS --------------*/
+
+typedef enum e_libft_error	t_libft_error;
+typedef enum e_bool			t_bool;
+
+/*-------------- FUNCTION PROTOTYPES -------------*/
 
 // libft mandatory
-int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t cnt, size_t size);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
 int		ft_isdigit(int c);
-int		ft_isprint(int c);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *str, size_t len);
-void	*ft_memset(void *b, int c, size_t len);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *haydequeue, const char *needle, size_t len);
-char	*ft_strrchr(const char *s, int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *s2);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-void	ft_putchar_fd(char c, int fd);
+int		ft_isalpha(int c);
+int		ft_abs(int x);
 void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
 
 // memory allocation
 void	*ft_malloc(size_t size_of);
 void	ft_exit(const char *str, int error_status);
-
-// math
-int		ft_max(int a, int b);
-int		ft_abs(int x);
 
 #endif
