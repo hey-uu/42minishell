e3cfa29af76ab0688a03020dddb4fc7001fddfcc
hey-uu <khy2714@gmail.com>
2023-01-10 21:56:32 +0900

    Fix: libadt Makefile 오타 수정 (v2.0)

--- lib/libadt/list/ft_lstlast.c
+++ lib/libadt/list/ft_lstlast.c
@@ -6,10 +6,11 @@
 /*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 14:01:46 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/10 19:11:01 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/10 21:28:37 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "list_internal.h"
 
 t_list	*ft_lstlast(t_list *lst)
