a54225f8916b79232dd5e10d7c6bdcd66449bd06
hey-uu <khy2714@gmail.com>
2023-01-18 03:54:31 +0900

    Rename: file name 수정 및 env_dup_val 함수 추가

--- mandatory/src/env_manager/internal_env_get.c
+++ mandatory/src/env_manager/internal_env_get.c
@@ -1,57 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_internal.c                                     :+:      :+:    :+:   */
+/*   internal_env_get.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 21:07:28 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/17 12:19:03 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/18 03:53:42 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env_manager_internal.h"
 #include "libft.h"
-
-t_env_tab	*__env_table_initialize__(t_env_tab *table, char **envp)
-{
-	char	*key;
-	char	*value;
-	size_t	i;
-	size_t	j;
-
-	if (!envp)
-		return (NULL);
-	hash_table_initialize(table);
-	i = 0;
-	while (envp[i])
-	{
-		j = 0;
-		while (envp[i][j] && envp[i][j] != '=')
-			j++;
-		key = ft_strndup(envp[i], j);
-		value = ft_strdup(&envp[i][j + 1]);
-		hash_table_insert(table, key, value);
-		i++;
-	}
-	return (table);
-}
-
-t_env_tab	*__env_set__(t_env_tab *table, char *variable, char *value)
-{
-	if (!variable)
-		return (NULL);
-	hash_table_update(table, variable, value);
-	return (table);
-}
-
-t_env_tab	*__env_unset__(t_env_tab *table, char *variable)
-{
-	if (!variable)
-		return (NULL);
-	hash_table_delete(table, variable);
-	return (table);
-}
 
 char	*__env_get__(t_env_tab *table, char *variable)
 {
