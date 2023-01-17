558c3caa7b5f8b4d7ce8c142fd7be4f4bb924898
yonaCluster <yonaimyo@gmail.com>
2023-01-18 01:25:24 +0900

    Feat: execute_subshell 함수 내용 작성

--- mandatory/src/executor/execute_subshell.c
+++ mandatory/src/executor/execute_subshell.c
@@ -5,20 +5,31 @@
 
 int	execute_subshell(t_node *node, char **envp, t_pipeline *pl, int nth)
 {
+	const int	last = (nth == pl->child_cnt);
 	int			pid;
+	int			exit_code;
 
-	// pid = fork();
-	// do_piping(pl->pipe_fd, nth);
-	// if (pid == 0)
-	// {
-	// 	do_redirecting(node->exe_unit->redir_list);
-	// 	executor(node->first_child, envp);
-	// }
-	// if (nth)
-	// {
-	// 	close(pl->pipe_fd[P_READ]);
-	// 	pl->nth_child_pid = pid;
-	// }
-	// close(pl->pipe_fd[P_WRITE]);
+	if (pl->child_cnt != 1 && !last)
+		open_new_pipe(pl->new_pipe_fd);
+	pid = fork();
+	if (pid < 0)
+		exit(1);
+	// set_standard_stream(pl, node->exe_unit->redir_list, nth);
+	else if (pid == 0)
+	{
+		set_standard_stream(pl, node->exe_unit->redir_list, nth);
+		exit_code = executor(node->first_child, envp);
+		printf("subshell child process status: %d", exit_code);
+		exit(0);
+	}
+	else
+	{
+		if (last)
+			pl->last_child_pid = pid;
+		close_pipe_in_parent(pl->old_pipe_fd, pl->new_pipe_fd, \
+													(nth == 1), last);
+		ft_memcpy(pl->old_pipe_fd, pl->new_pipe_fd, sizeof(int[2]));
+	}
 	return (SUCCESS);
 }
+
