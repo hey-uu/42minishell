3220e9df8d9b44de45a97f54b20a5bef19a4a018
yonaCluster <yonaimyo@gmail.com>
2023-01-18 01:23:51 +0900

    Test: 디버깅을 위한 built-in 함수 수정

--- mandatory/src/executor/builtin.c
+++ mandatory/src/executor/builtin.c
@@ -1,29 +1,37 @@
+#include <stdio.h>
+
 int	builtin_echo(char **argv, char **envp)
 {
+	printf("this is built-in echo\n");
 	return 0;
 }
 
 int	builtin_cd(char **argv, char **envp)
 {
+	printf("this is built-in cd\n");
 	return 0;
 }
 
 int	builtin_pwd(char **argv, char **envp)
 {
+	printf("this is built-in pwd\n");
 	return 0;
 }
 
 int	builtin_export(char **argv, char **envp)
 {
+	printf("this is built-in export\n");
 	return 0;
 }
 
 int	builtin_env(char **argv, char **envp)
 {
+	printf("this is built-in env\n");
 	return 0;
 }
 
 int	builtin_exit(char **argv, char **envp)
 {
+	printf("this is built-in exit\n");
 	return 0;
 }
