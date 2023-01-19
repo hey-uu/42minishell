57db3e01c1f57792bfe25b1e3203ece08cbd96af
yonaCluster <yonaimyo@gmail.com>
2023-01-19 12:55:07 +0900

    Struct: 에러 처리 로직 변경

--- mandatory/src/extra/error.c
+++ mandatory/src/extra/error.c
@@ -6,22 +6,33 @@
 /*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 13:52:11 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/17 23:38:50 by yeonhkim         ###   ########.fr       */
+/*   Updated: 2023/01/19 12:44:48 by yeonhkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error.h"
+#include "minishell.h"
 
-void	print_syntax_error_message(int token_type, char *token_str)
+void	print_syntax_error_message(t_token token)
 {
-	static char	*type_str[11] = {"newline", 0, "(", ")", "&&", "||", \
+	static char	*token_type_str[11] = {"newline", 0, "(", ")", "&&", "||", \
 								"|", "<", ">", "<<", ">>"};
 
 	printf("goldsh: syntax error near unexpected token ");
-	if (token_type == TOKEN_WORD)
-		printf("'%s'\n", token_str);
+	if (token.type == TOKEN_WORD)
+		printf("'%s'\n", token.str);
 	else
-		printf("'%s'\n", type_str[token_type]);
+		printf("'%s'\n", token_type_str[token.type]);
+}
+
+void	handle_error(int errcode, t_token syntax_error_near_token)
+{
+	if (errcode == 0)
+		return ;
+	else if (errcode == ERROR_IN_SYNTAX)
+		print_syntax_error_message(syntax_error_near_token);
+	dprintf(2, "Error! This errorcode is... %d\n", errcode);
+	exit(1);
 }
 
 void	exit_by_error(char *msg)
