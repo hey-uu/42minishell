759dc59173ecdcffcc78e66d55f3bfe0bbf2c9bd
hey-uu <khy2714@gmail.com>
2023-01-21 03:27:34 +0900

    Refactor: builtin_exit error case 추가(v2.0)

--- mandatory/src/builtin/builtin_exit.c
+++ mandatory/src/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 22:24:02 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/21 01:56:51 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/21 03:11:23 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,46 @@
 #include "error.h"
 #include "libft.h"
 
+static int	__is_numeric_argument__(char *argument)
+{
+	int	i;
+
+	i = 0;
+	while (argument[i])
+	{
+		if (!ft_isdigit(argument[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	__exit_error_handler__(int error_number, char ***argv, char *arg)
+{
+	print_builtin_error_message(error_number, BUILTIN_EXIT, arg);
+	free_double_char_array(argv);
+	exit_stat_update(2);
+	exit_program();
+	return (BUILTIN_FAIL);
+}
+
+
 int	builtin_exit(char *argv[])
 {
 	printf("exit\n");
+	if (argv[1] && (!__is_numeric_argument__(argv[1])))
+	{
+		return (__exit_error_handler__(ERROR_NOT_NUMBER, &argv, argv[1]));
+	}
 	if (argv[1] && argv[2])
 	{
-		free_double_char_array(&argv);
-		exit_stat_update(1);
-		exit_program();
+		return (__exit_error_handler__(ERROR_TOO_MANY_ARGUMENTS, &argv, NULL));
 	}
 	if (argv[1])
+	{
 		exit_stat_update(ft_atoi(argv[1]));
+	}
 	free_double_char_array(&argv);
 	exit_program();
+	return (BUILTIN_SUCCESS);
 }
