8f668f22b8122177f97b6d668a9be53104cc79e7
hey-uu <khy2714@gmail.com>
2023-01-24 17:47:09 +0900

    Refactor: libadt queue의 create_queue 함수에서 초기화하는 부분은 따로 함수로 뺌

--- lib/libadt/queue/queue_create.c
+++ lib/libadt/queue/queue_create.c
@@ -6,7 +6,7 @@
 /*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 06:16:39 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/17 11:52:37 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/24 00:44:56 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,8 @@
 #include "t_queue.h"
 #include "libft.h"
 
-t_queue	*create_queue(int initial_size, int type)
+void	init_queue(t_queue *queue, int initial_size, int type)
 {
-	t_queue	*queue;
-
-	queue = ft_malloc(sizeof(t_queue));
 	queue->type = type;
 	queue->size = initial_size;
 	queue->used_size = 0;
@@ -32,5 +29,13 @@ t_queue	*create_queue(int initial_size, int type)
 		queue->strarr = ft_calloc(queue->size, sizeof(char *));
 	else
 		queue->strarr = NULL;
+}
+
+t_queue	*create_queue(int initial_size, int type)
+{
+	t_queue	*queue;
+
+	queue = ft_malloc(sizeof(t_queue));
+	init_queue(queue, initial_size, type);
 	return (queue);
 }
