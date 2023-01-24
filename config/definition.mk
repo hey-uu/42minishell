5410b8fd41f9a42a5d0462e935f316fb7350878b
yonaCluster <yonaimyo@gmail.com>
2023-01-25 05:22:51 +0900

    Rename: access_command_path -> access_command 파일명 변경makefile에 반영

--- config/definition.mk
+++ config/definition.mk
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    definition.mk                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+         #
+#    By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/05 11:45:38 by hyeyukim          #+#    #+#              #
-#    Updated: 2023/01/21 03:32:52 by hyeyukim         ###   ########.fr        #
+#    Updated: 2023/01/24 21:23:42 by yeonhkim         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -113,7 +113,7 @@ BUILTIN_FILE	=		builtin_cd \
 						builtin_env \
 						builtin_exit \
 						builtin_export
-EXECUTOR_FILE	=		access_command_path \
+EXECUTOR_FILE	=		access_command \
 						execute_builtin \
 						execute_pipeline \
 						execute_list \
