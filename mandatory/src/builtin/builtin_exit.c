628a4a8460231fa9554e022d0265339b314c248c
hey-uu <khy2714@gmail.com>
2023-01-25 08:26:26 +0900

    Refactor: builtin exit 에러에 따라 종료 상태 달라지도록 수정

--- mandatory/src/builtin/builtin_exit.c
+++ mandatory/src/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 22:24:02 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/25 00:31:39 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/25 03:11:58 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,19 @@ static int	is_numeric_argument(char *argument)
 	return (1);
 }
 
-static int	exit_error_handler(int error_number, char ***argv, char *arg)
+static void	exit_error_handler(int error_number, char ***argv, char *arg)
 {
 	print_builtin_error_message(error_number, CMD_EXIT, arg);
 	free_double_char_array(argv);
-	exit_stat_update(2);
-	exit_program();
-	return (BUILTIN_FAIL);
+	if (error_number == BERR_NOT_NUMBER)
+	{
+		exit_stat_update(255);
+		exit_program();
+	}
+	else if (error_number == BERR_TOO_MANY_ARGUMENTS)
+	{
+		exit_stat_update(1);
+	}
 }
 
 void	builtin_exit(char *argv[])
@@ -46,10 +52,13 @@ void	builtin_exit(char *argv[])
 	printf("exit\n");
 	if (argv[1] && (!is_numeric_argument(argv[1])))
 		exit_error_handler(BERR_NOT_NUMBER, &argv, argv[1]);
-	if (argv[1] && argv[2])
+	else if (argv[1] && argv[2])
 		exit_error_handler(BERR_TOO_MANY_ARGUMENTS, &argv, NULL);
-	if (argv[1])
-		exit_stat_update(ft_atoi(argv[1]));
-	free_double_char_array(&argv);
-	exit_program();
+	else
+	{
+		if (argv[1])
+			exit_stat_update(ft_atoi(argv[1]));
+		free_double_char_array(&argv);
+		exit_program();
+	}
 }
