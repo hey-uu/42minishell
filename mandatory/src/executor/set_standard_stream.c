141715026d886a70ad501e045c17156d1a91afd7
hey-uu <khy2714@gmail.com>
2023-01-25 16:35:58 +0900

    Refactor: do_redirecting ambiguous redirect 에러 처리

--- mandatory/src/executor/set_standard_stream.c
+++ mandatory/src/executor/set_standard_stream.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   set_standard_stream.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 16:24:28 by yeonhkim          #+#    #+#             */
-/*   Updated: 2023/01/19 20:33:26 by yeonhkim         ###   ########.fr       */
+/*   Updated: 2023/01/25 16:06:34 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 #include "executor.h"
 #include "minishell.h"
 #include "parser.h"
+#include "env_manager.h"
 
-static void	do_redirecting(t_redir *redir_list)
+int	do_redirecting(t_redir *redir_list)
 {
 	int	fd;
 	int	err;
 	int	i;
 
 	i = 0;
-	while (redir_list[i].str)
+	while (redir_list[i].num != TOKEN_NONE)
 	{
 		if (redir_list[i].num == TOKEN_REDIR_IN \
 				|| redir_list[i].num == TOKEN_REDIR_IN_HERE)
@@ -38,10 +39,20 @@ static void	do_redirecting(t_redir *redir_list)
 						O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			err = dup2(fd, STDOUT_FILENO);
 		}
+		else if (redir_list[i].num == ERR_EXE_AMBIGUOUS_REDIR)
+		{
+			handle_execute_error(\
+			ERR_EXE_AMBIGUOUS_REDIR, NULL, redir_list[i].str);
+			return (ERR_EXE_AMBIGUOUS_REDIR);
+		}
 		if (err < 0)
+		{
 			exit_by_error("dup2 error");
+			return (ERR_EXE_SYSCALL_FAILED);
+		}
 		i++;
 	}
+	return (ERR_EXE_NONE);
 }
 
 static void	do_piping(int old_pipe_fd[2], int new_pipe_fd[2], \
@@ -77,5 +88,6 @@ void	set_standard_stream(t_pipeline *pl, t_redir *redir_list, int nth)
 {
 	if (pl && pl->pipe_exist)
 		do_piping(pl->old_pipe_fd, pl->new_pipe_fd, nth, pl->child_cnt);
-	do_redirecting(redir_list);
+	if (do_redirecting(redir_list) > 0)
+		exit_program();
 }
