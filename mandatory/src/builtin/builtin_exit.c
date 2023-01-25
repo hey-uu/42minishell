cde9df8a1bd869778d8cf3230cc2ba0395e88f7b
hey-uu <khy2714@gmail.com>
2023-01-25 13:08:03 +0900

    Fix: builtin_exit argv free 해주는 위치 변경

--- mandatory/src/builtin/builtin_exit.c
+++ mandatory/src/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 22:24:02 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/25 13:00:07 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/25 13:05:56 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,8 @@ static void	exit_terminate_with_no_error(char ***argv, char *arg, int exit_stat)
 
 static void	exit_terminate_with_error(char ***argv, char *arg, int errcode)
 {
-	free_double_char_array(argv);
 	handle_builtin_error(errcode, CMD_EXIT, arg);
+	free_double_char_array(argv);
 	if (errcode == ERR_B_NOT_NUMBER)
 		exit_program();
 }
