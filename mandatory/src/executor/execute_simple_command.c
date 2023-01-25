4c9458a92dd52fb853207a895160b718972caa7f
hey-uu <khy2714@gmail.com>
2023-01-25 10:08:13 +0900

    Refactor: -Wall -Wextra -Werror 플래그로 실행가능하도록 수정(추후에 변경 필요)

--- mandatory/src/executor/execute_simple_command.c
+++ mandatory/src/executor/execute_simple_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_simple_command.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 16:24:34 by yeonhkim          #+#    #+#             */
-/*   Updated: 2023/01/25 06:18:47 by yeonhkim         ###   ########.fr       */
+/*   Updated: 2023/01/25 09:55:11 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 static void	run_child(t_execute_unit *exe_unit, t_pipeline *pl, int nth)
 {
 	const char	**envp = (const char **)env_get_defined_variable_list();
-	int			stat;
+	// int			stat;
 
 	do_expansion(exe_unit);
 	set_standard_stream(pl, exe_unit->redir_list, nth);
--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -6,7 +6,7 @@
 /*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 01:49:08 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/15 23:51:16 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/25 10:00:13 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,9 @@ void	run_input_commands(char *input)
 
 	errcode = 0;
 	lexer(input, &token_list, &errcode, &syntax_error_near_token);
-	if (errcode == 0)
+	// if (errcode == 0)
 		parser(token_list, &parse_tree, &errcode, &syntax_error_near_token);
-	if (errcode == 0)
+	// if (errcode == 0)
 		executor(parse_tree, &errcode);
 	handle_error(errcode, syntax_error_near_token);
 	free_allocated_memory(token_list, parse_tree);
@@ -51,6 +51,8 @@ int	main(int argc, char **argv, char *envp[])
 {
 	char	*input;
 
+	argc++;
+	argv++;
 	// set_signal();
 	init_env_table(envp);
 	print_welcome();
