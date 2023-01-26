3ba87d0818b985155bdaa2e9f1870f4a93cb66a3
yonaCluster <yonaimyo@gmail.com>
2023-01-26 20:45:47 +0900

    Chore: 필요없는 부분 삭제

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 01:49:08 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/26 20:11:15 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/26 20:43:53 by yeonhkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,56 +21,16 @@
 #include "executor.h"
 #include "signal_handle.h"
 
-// 처리해야할 것
-// * signal 설정
-// * history 업데이트
-// * executor 함수
-
-
-// #include <termios.h>
-
-// enum e_termios {
-// 	ORI,
-// 	DEF
-// };
-
-// enum e_terminal_set {
-// 	CANONICAL,	
-// 	NON_CANONICAL
-// };
-
-// void	set_terminal(int input_mode)
-// {
-// 	static struct termios	ori_term;
-// 	static struct termios	def_term;
-// 	static int				called;
-
-// 	if (!called)
-// 	{
-// 		tcgetattr(STDIN_FILENO, &ori_term);
-// 		def_term.c_lflag &= ~(ICANON | ECHO);
-// 		def_term.c_cc[VMIN] = 1; 
-// 		def_term.c_cc[VTIME] = 0;
-// 		called = 1;
-// 	}
-// 	if (input_mode == CANONICAL)
-// 		tcsetattr(STDIN_FILENO, TCSANOW, &ori_term);
-// 	else
-// 		tcsetattr(STDIN_FILENO, TCSANOW, &def_term);
-// }
-
 void	init_shell(char *envp[])
 {
 	print_welcome();
 	set_signal(CATCH, CATCH);
-	// set_terminal(NON_CANONICAL);
 	init_env_table(envp);
 }
 
 int	main(int argc, char **argv, char *envp[])
 {
 	char			*input;
-	// struct termios	ori_term;
 
 	argc = 0;
 	argv = NULL;
@@ -83,7 +43,6 @@ int	main(int argc, char **argv, char *envp[])
 		add_history(input);
 		run_commands(input);
 		free(input);
-		system("leaks minishell");
+		// system("leaks minishell");
 	}
-	// set_terminal(CANONICAL);
 }
