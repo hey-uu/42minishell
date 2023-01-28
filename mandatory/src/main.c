f4227a53cebaa4e5dda2011c17d392b930d32d52
yonaCluster <yonaimyo@gmail.com>
2023-01-28 12:47:35 +0900

    프롬프트 공백 추가

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeyukim <hyeyukim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 01:49:08 by hyeyukim          #+#    #+#             */
-/*   Updated: 2023/01/27 20:37:59 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2023/01/28 12:47:15 by yeonhkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	main(int argc, char **argv, char *envp[])
 	init_shell(envp);
 	while (1)
 	{
-		input = readline("goldsh $");
+		input = readline("goldsh $ ");
 		if (!input)
 			builtin_exit(NULL);
 		add_history(input);
