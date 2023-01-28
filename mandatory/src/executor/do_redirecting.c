78180c4c8e609f7a844b0b9ec3e9641b2100932c
yonaCluster <yonaimyo@gmail.com>
2023-01-28 19:55:51 +0900

    Chore: do_redirecting.c 필요없는 부분 삭제

--- mandatory/src/executor/do_redirecting.c
+++ mandatory/src/executor/do_redirecting.c
@@ -6,23 +6,13 @@
 /*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 19:08:41 by yeonhkim          #+#    #+#             */
-/*   Updated: 2023/01/28 19:46:23 by yeonhkim         ###   ########.fr       */
+/*   Updated: 2023/01/28 19:54:30 by yeonhkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include "wrapped_syscall.h"
 #include "executor_internal.h"
-
-static int	io_type(int redir_type)
-{
-	if (redir_type == REDIR_IN || redir_type == REDIR_HEREDOC)
-		return (IN);
-	else if (redir_type == REDIR_OUT || redir_type == REDIR_APPEND)
-		return (OUT);
-	else
-		return (-1);
-}
 
 static int	redirect_input(char *filename, int redir_type)
 {
@@ -80,33 +70,13 @@ static int	process_redirect(int redir_type, char *filename)
 
 int	do_redirecting(t_redir *redir_list)
 {
-	int					prev_std_fd[2];
-	int					i;
+	int	i;
 
-	prev_std_fd[IN] = 0;
-	prev_std_fd[OUT] = 0;
 	i = 0;
 	while (redir_list[i].num != REDIR_NONE)
 	{
-		// dprintf(2, "prev in: %d, prev out: %d\n", prev_std_fd[IN], prev_std_fd[OUT]);
-		{
-			if (io_type(redir_list[i].num) == IN && prev_std_fd[IN] != 0)
-			{
-				// dprintf(2, "close fd: %d\n", prev_std_fd[IN]);
-				close(prev_std_fd[IN]);
-			}
-			else if (io_type(redir_list[i].num) == OUT && prev_std_fd[OUT] != 0)
-			{
-				// dprintf(2, "close fd: %d\n", prev_std_fd[OUT]);
-				close(prev_std_fd[OUT]);
-			}
-		}
-		if (process_redirect(redir_list[i].num, redir_list[i].str) == SUCCESS)
-		{
-			;
-			// prev_std_fd[IN] = dup(STDIN_FILENO);
-			// prev_std_fd[OUT] = dup(STDOUT_FILENO);
-		}
+		if (process_redirect(redir_list[i].num, redir_list[i].str) == FAILURE)
+			return (FAILURE);
 		i++;
 	}
 	return (SUCCESS);
