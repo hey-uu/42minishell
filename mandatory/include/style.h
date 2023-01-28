f3a0c9cd69ad8b6dd91a2b89d7b75b86f78d4db7
yonaCluster <yonaimyo@gmail.com>
2023-01-28 15:31:07 +0900

    Norm: style.h 수정

--- mandatory/include/style.h
+++ mandatory/include/style.h
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   color.h                                            :+:      :+:    :+:   */
+/*   style.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yona <yona@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: yeonhkim <yeonhkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/19 13:05:53 by yeonhkim          #+#    #+#             */
-/*   Updated: 2023/01/28 03:12:33 by yona             ###   ########.fr       */
+/*   Created: 2023/01/28 15:22:05 by yeonhkim          #+#    #+#             */
+/*   Updated: 2023/01/28 15:25:41 by yeonhkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	STYLE_H
+#ifndef STYLE_H
 # define STYLE_H
 
 /*------------ DEFINE MACRO CONSTANTS ------------*/
 
-/* ANSI Escape codes defines print style */
+// ANSI Escape codes defines print style
+
+/* font style */
 # define RESET		"\x1B[0m"
 # define BOLD		"\x1B[1m"
 # define FAINT		"\x1B[2m"
 # define ITAL		"\x1B[3m"
+
+/* color */
 # define RED		"\x1B[31m"
 # define GREEN		"\x1B[32m"
 # define YELLO		"\x1B[33m"
